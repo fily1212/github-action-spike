40fa607fb5442009026309b99379b8ea82fe51ed
Fily1212 <Fily1212>
2025-05-21 11:41:08 +0200

    progetto

---
+++ src/main/java/it/unito/githubactionspike/Controller.java
@@ -0,0 +1,12 @@
+package it.unito.githubactionspike;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping
+    public String hello() {
+        return "Hello, World!";
+    }
+}
---
+++ src/main/java/it/unito/githubactionspike/GithubActionSpikeApplication.java
@@ -0,0 +1,13 @@
+package it.unito.githubactionspike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionSpikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionSpikeApplication.class, args);
+    }
+
+}
